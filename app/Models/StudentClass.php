1b75c544d833cfe9ab9236adb6294888c990d100
FreshDev <billyhans90@gmail.com>
2022-05-13 11:30:53 +0100

    models and migrations, done

--- app/Models/StudentClass.php
+++ app/Models/StudentClass.php
@@ -8,6 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class StudentClass extends Model
 {
     use HasFactory;
+
+    protected $table = 'student_classes';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'class',
+    ];
+
     public function studentRelation()
     {
         return $this->hasMany(Student::class, 'class_id', 'id');
